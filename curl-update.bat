0ac89e57384be1e2f558f3beec895315a2899aa7
dashi <Vyalko67@gmail.com>
2025-06-20 19:40:10 +0300

    Update curl-update.bat

--- curl-update.bat
+++ curl-update.bat
@@ -30,7 +30,7 @@ copy /V /Y .\curl-temp\curl.exe ".\"
 copy /V /Y .\curl-temp\curl-ca-bundle.crt ".\"
 
 :: install curl certificate
-certutil -addstore -f CA .\curl-ca-bundle.crt
+certutil -addstore -f "Root" .\curl-ca-bundle.crt
 
 del /q .\curl-temp.zip
 rd /s /q .\curl-temp
